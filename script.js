00b174088637cd69523e09624e72eea2f1f6271c
Bruno000 <73971514+Bruno000@users.noreply.github.com>
2021-03-22 12:44:31 -0300

    Revert "bruno007"
    This reverts commit 1140d530af3406d76099c7c48521f8284ba1476f.

--- script.js
+++
@@ -1,8 +0,0 @@
-var hamburguer = document.querySelector(".hamburguer");
-
-hamburguer.addEventListener("click", function() {
-    document.querySelector(".sidebar").classList.toggle("show-menu");
-});
-hamburguer.addEventListener("click", function() {
-    document.querySelector(".hamburguer").classList.toggle("show-menu");
-});
